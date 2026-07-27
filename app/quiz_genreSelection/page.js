ab0ec8e6558d40e8bf8d9b7579afe9847aee90da
Zue Zue Lwin <251012@st.yoshida-g.ac.jp>
2026-07-27 15:13:12 +0900

    module.css

--- app/quiz_genreSelection/page.js
+++ app/quiz_genreSelection/page.js
@@ -1,30 +1,30 @@
 import Link from "next/link";
-import "./style.css";
+import styles from "./style.module.css";
 
 export default function QuizGenre() {
   return (
-    <div className="page">
-      <div className="top">
-        <Link href="/" className="menu">
+    <div className={styles.page}>
+      <div className={styles.top}>
+        <Link href="/" className={styles.menu}>
           戻る
         </Link>
 
-        <div className="title">クイズ</div>
-        <div className="title">ジャンル選択</div>
+        <div className={styles.title}>クイズ</div>
+        <div className={styles.title}>ジャンル選択</div>
       </div>
 
-      <div className="content">
-        <Link href="/quiz_stageSelection?genreId=1" className="genreBox">
-          <div className="genreName">ジャンル1</div>
-          <div className="starArea">
+      <div className={styles.content}>
+        <Link href="/quiz_stageSelection?genreId=1" className={styles.genreBox}>
+          <div className={styles.genreName}>ジャンル１</div>
+          <div className={styles.starArea}>
             <div>☆</div>
             <div>0個/0個</div>
           </div>
         </Link>
 
-        <Link href="/quiz_stageSelection?genreId=2" className="genreBox">
-          <div className="genreName">ジャンル2</div>
-          <div className="starArea">
+        <Link href="/quiz_stageSelection?genreId=2" className={styles.genreBox}>
+          <div className={styles.genreName}>ジャンル２</div>
+          <div className={styles.starArea}>
             <div>☆</div>
             <div>0個/0個</div>
           </div>
--- app/quiz_stageSelection/page.js
+++ app/quiz_stageSelection/page.js
@@ -1,51 +1,63 @@
 import Link from "next/link";
-import "./style.css";
+import styles from "./style.module.css";
 
-export default function QuizStage() {
+// ★ export default function にすることが一番重要です！
+export default function QuizStageSelection() {
   return (
-    <div className="page">
-      <div className="top">
-        <Link href="/quiz_genreSelection">
-          <button className="menu">戻る</button>
+    <div className={styles.page}>
+      {/* ヘッダー部分 */}
+      <div className={styles.top}>
+        <Link href="/quiz_genre" className={styles.menu}>
+          戻る
         </Link>
-
-        <div className="title">クイズ</div>
-        <div className="title">ジャンル名</div>
-        <div className="title">ステージ選択</div>
+        <div className={styles.title}>クイズ</div>
+        <div className={styles.title}>ジャンル名</div>
+        <div className={styles.title}>ステージ選択</div>
       </div>
 
-      <div className="content">
-
-        <Link href="/quiz_question" className="link">
-          <div className="stageBox">
-            <div className="stage">ステージ1</div>
-
-            <div className="mode">ノーマル</div>
-
-            <div className="mode">ハード</div>
-
-            <div className="result">
-              <div className="star">☆☆☆</div>
-              <div>クリア / 全問正解 / スピード</div>
+      {/* メインコンテンツ */}
+      <div className={styles.content}>
+        {/* ステージ１ */}
+        <Link href="/quiz_play?stageId=1" className={styles.link}>
+          <div className={styles.stageBox}>
+            <div className={styles.stage}>ステージ１</div>
+            <div className={styles.mode}>ノーマル</div>
+            <div className={styles.mode}>ハード</div>
+            <div className={styles.result}>
+              <div className={styles.starArea}>
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+              </div>
+              <div className={styles.labelArea}>
+                <span>クリア</span>
+                <span>全問正解</span>
+                <span>スピード</span>
+              </div>
             </div>
           </div>
         </Link>
 
-        <Link href="/quiz_question" className="link">
-          <div className="stageBox">
-            <div className="stage">ステージ2</div>
-
-            <div className="mode">ノーマル</div>
-
-            <div className="mode">ハード</div>
-
-            <div className="result">
-              <div className="star">☆☆☆</div>
-              <div>クリア / 全問正解 / スピード</div>
+        {/* ステージ２ */}
+        <Link href="/quiz_play?stageId=2" className={styles.link}>
+          <div className={styles.stageBox}>
+            <div className={styles.stage}>ステージ２</div>
+            <div className={styles.mode}>ノーマル</div>
+            <div className={styles.mode}>ハード</div>
+            <div className={styles.result}>
+              <div className={styles.starArea}>
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+              </div>
+              <div className={styles.labelArea}>
+                <span>クリア</span>
+                <span>全問正解</span>
+                <span>スピード</span>
+              </div>
             </div>
           </div>
         </Link>
-
       </div>
     </div>
   );
